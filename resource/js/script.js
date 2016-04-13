f57f1e1103c460aedd0c4fa28270bc9272d2b8cc
andrew charlebois <andrew.charlebois@hotmail.com>
2016-04-12 23:47:50 -0400

    add
    added waypoints.js, work in progress

---
+++ resource/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    /* SMOOTH SCROLL */
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+
+    /* For sticky nav */
+    $('.js--about-me').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
+    
+
+    /* scroll on btn */
+    $('.js--scroll-next-page').click(function (){
+        $('html, body').animate({
+            scrollTop: $('.js--about-me').offset().top
+        }, 1000);
+    });
+
+
+
+
+});
