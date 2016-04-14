dedbfd10d2c18922515d4ed21591c4216933ce7c
andrew charlebois <andrew.charlebois@hotmail.com>
2016-04-14 01:04:51 -0400

    add/remove
    added compressed images to replace current. added all media queries.
    modified grid.css responsive layout to stack layout at 600px instead of
    480px. added animated.css.

--- resource/js/script.js
+++ resource/js/script.js
@@ -35,8 +35,52 @@ $(document).ready(function() {
             scrollTop: $('.js--about-me').offset().top
         }, 1000);
     });
-
-
-
+    
+/* animate on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '59%'
+    })
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeIn');
+    }, {
+        offset: '59%'
+    })
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '59%'
+    })
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated fadeIn');
+    }, {
+        offset: '59%'
+    })
+    
+    $('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated fadeIn');
+    }, {
+        offset: '59%'
+    })
+    
+    /* Mobile nav */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200)
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
 
 });
